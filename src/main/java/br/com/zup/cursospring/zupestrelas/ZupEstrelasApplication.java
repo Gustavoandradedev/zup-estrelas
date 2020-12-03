7f87f6462533d4a7ae9871348e9f9cb3f4457389
Gustavo Andrade <gustavoandrade15@hotmail.com>
2020-12-02 22:17:25 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/br/com/zup/cursospring/zupestrelas/ZupEstrelasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.cursospring.zupestrelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupEstrelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupEstrelasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/cursospring/zupestrelas/ZupEstrelasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.cursospring.zupestrelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupEstrelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
